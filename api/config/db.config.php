69b8e8f00e00289480fd7cf2fdb772d2702af257
yeliex <yeliex@yeliex.com>
2015-09-11 00:53:30 +0800

    初始化发布版本

--- api/config/db.config.php
+++ api/config/db.config.php
@@ -2,5 +2,5 @@
 	define('DB_HOST','mizhi.pub');
 	define('DB_USER','yeliex');
 	define('DB_PASSWD','htsxking');
-	define('DB_DBNAME','mizhi_test');
+	define('DB_DBNAME','mizhi_app');
 ?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yeliex
- * Date: 15/8/28
- * Time: 上午11:53
- */
-$time = date('Ymdhis000');
-print_r(date('y.m.d'));
