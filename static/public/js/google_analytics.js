0a45391c7fb49c7f2bae2e65b2f035259cf80253
yeliex <yeliex@yeliex.com>
2015-09-16 12:38:02 +0800

    修改了google analytics,现在支持多个域名统计

--- static/public/js/google_analytics.js
+++ static/public/js/google_analytics.js
@@ -1,3 +1,1 @@
-/**
- * Created by yeliex on 15/9/15.
- */
+(function(d,e,j,h,f,c,b){d.GoogleAnalyticsObject=f;d[f]=d[f]||function(){(d[f].q=d[f].q||[]).push(arguments)},d[f].l=1*new Date();c=e.createElement(j),b=e.getElementsByTagName(j)[0];c.async=1;c.src=h;b.parentNode.insertBefore(c,b)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-67602772-1","auto");ga("send","pageview");
