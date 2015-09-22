9941461ab2c41ce48393fbc230231565cf0c3b49
yeliex <yeliex@yeliex.com>
2015-09-22 14:06:55 +0800

    正式发布新版首页

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yeliex
- * Date: 15/8/28
- * Time: 上午11:53
- */
-//
-//
-//$time = date('Ymdhis000');
-//print_r(date('y.m.d'));
-//
-//$a = array();
-//$a['to'] = array('yeliex@yeliex.com');
-//$a['sub'] = array();
-//$a['sub']['user'] = array('yeliex');
-//$a['sub']['phone'] = array('15988545765');
-//$a['sub']['contact_phone'] = array('87172313');
-//$a['sub']['mail'] = array('yeliex@yeliex.com');
-//$a['sub']['address'] = array('杭州市');
-//
-//
-//print_r(json_encode($a));
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript">
+        location.href=location.origin;
+    </script>
+</head>
+<body class="ye-index">
+</body>
+</html>
--- web/index/index.php
+++ web/index/index.php
@@ -1,7 +1,10 @@
 <?php
 require_once "index.html";
+include_once "contents.html";
 require_once "footer.html";
 ?>
+<script src="http://static.dev.mzapp.info/client/js/index.js" type="text/javascript"></script>
+<script src="http://static.dev.mzapp.info/public/js/yCarousel.js" type="text/javascript"></script>
 <script src="http://static.mzapp.info/public/js/google_analytics.js" type="text/javascript"></script>
 </body>
 </html>
